2c118ce0c1e5090018db9065da1d10fd2c1bc920
Nisarg <nisarg0808@gmail.com>
2024-02-11 19:17:11 -0500

    Provided intent in main activity on clicking button

--- app/src/main/java/com/example/project1_group9/MainActivity.kt
+++ app/src/main/java/com/example/project1_group9/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.project1_group9
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, ProductActivity::class.java)
-        startActivity(intent)
+        val txt1 = findViewById<TextView>(R.id.txt1)
+        val imgbtn = findViewById<ImageButton>(R.id.imgbtn)
 
-        // Finish MainActivity to prevent going back to it when pressing back button
-        finish()
+        imgbtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ProductActivity::class.java))
+        }
+        txt1.setOnClickListener{
+            startActivity(Intent(this@MainActivity, ProductActivity::class.java))
+        }
     }
 }
