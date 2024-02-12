b7c744ea824a131303aa81f1eb2959e47a9ab108
priyamanochamalhi <146389300+priyamanochamalhi@users.noreply.github.com>
2024-02-12 13:02:41 -0500

    renamed product.kt to Product.kt

---
+++ app/src/main/java/com/example/project1_group9/Product.kt
@@ -0,0 +1,5 @@
+package com.example.project1_group9
+
+data class Product(var name: String = "", var manufacturer: String="", var url: String="", var description: String="", var price: Double = 0.0)
+{
+}
--- app/src/main/java/com/example/project1_group9/product.kt
+++
@@ -1,5 +0,0 @@
-package com.example.project1_group9
-
-data class product(var name: String = "", var manufacturer: String="", var url: String="", var description: String="", var price: Double = 0.0)
-{
-}
